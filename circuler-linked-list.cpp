4314458c704462672c1c72966506d025fda42311
haris78866 <arainharis151@gmail.com>
2024-07-22 22:53:53 +0500

    ADD

--- circuler-linked-list.cpp
+++ circuler-linked-list.cpp
@@ -2,8 +2,43 @@
 using namespace std;
 
  struct node{
+int info;
+node *next;
+};
+
+
+class list {
+private:
+node *head,*temp,*temp1;
+public:
+
+list();
+~list();
+void option();
+void add();
+void remove();
+void print();
+void length();
 
 };
+
+// constructer 
+list :: list(){
+head=temp=temp1=NULL;
+}
+
+// destructer 
+list :: ~list(){
+temp=head;
+while(temp->next!=head){
+temp1=temp;
+temp=temp->next;
+delete temp1;
+}
+
+}
+
+
 
 
 int main(){
