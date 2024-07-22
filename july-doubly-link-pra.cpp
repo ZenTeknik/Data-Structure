6f28d02f551fff9b523ee620931e0d2b802fe774
haris78866 <arainharis151@gmail.com>
2024-07-22 14:32:27 +0500

    ADD

--- july-doubly-link-pra.cpp
+++ july-doubly-link-pra.cpp
@@ -27,12 +27,13 @@ void search();
 
 };
 
-
+// constructer 
 list :: list(){
     head=temp=temp1=min=max=NULL;
-
 }
 
+
+// destructer
 list :: ~list(){
     temp=head;
     while(temp!=NULL){
@@ -42,23 +43,52 @@ list :: ~list(){
     }
 }
 
-
+// function for option
 void list :: option(){
 
 int choice;
 do{
 cout<<"Avalible Option  :"<<endl;
 cout<<"1) print : "<<endl;
-cout<<"2) print : "<<endl;
+cout<<"2) add : "<<endl;
+cout<<"3) remove : "<<endl;
+cout<<"4) search : "<<endl;
+cout<<"5) minimum : "<<endl;
+cout<<"6) maximum : "<<endl;
+cout<<"7) length : "<<endl;
+cout<<"0) Exit : "<<endl;
+cout<<"Enter your choice : "<<endl;
+cin>>choice;
 
+if(choice==1){
 
+}
+else if(choice==2){
 
+}
+else if(choice==3){
+    
+}
+else if(choice==4){
+    
+}
+else if(choice==5){
+    
+}
+else if(choice==6){
+    
+}
+else if(choice==7){
+    
+}
+else if(choice==0){
+    
+}
+else {
+    cout<<"Invalid Choice : "<<endl;
+}
 }while(choice!=0);
 
-
-
-
-int main(){
-
-return 0;
 }
+
+
