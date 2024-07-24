09b25fbc7551ef0d69ba74dc8c72ef88622d811d
haris78866 <arainharis151@gmail.com>
2024-07-24 23:45:30 +0500

    ADD

---
+++ stack-operation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+struct node {
+int info;
+node *next;
+};
+
+
+
+class list {
+private: 
+node *top,*temp;
+public: 
+
+
+
+};
+
+int main(){
+
+
+return 0;
+}
