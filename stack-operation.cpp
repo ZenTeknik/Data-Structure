c3b910ca2c186b65e0df51efabbcaa7e665b505b
haris78866 <arainharis151@gmail.com>
2024-07-25 00:03:27 +0500

    NEW

--- stack-operation.cpp
+++ stack-operation.cpp
@@ -12,10 +12,61 @@ class list {
 private: 
 node *top,*temp;
 public: 
-
-
-
+list();
+~list();
+void option();
+void push();
+void pop();
 };
+
+
+// constructer 
+list :: list(){
+int count=0;
+int limit=10;
+top=temp=NULL;
+}
+
+
+void list :: option(){
+int choice;
+do{
+cout<<"Avalible option : "<<endl;
+cout<<"1) push : "<<endl;
+cout<<"2) Pop  : "<<endl;
+cout<<"0) Exit : "<<endl;
+cout<<"Enter your choice : "<<endl;
+cin>>choice;
+
+switch(choice){
+case 1: 
+push();
+break;
+
+
+case 2: 
+pop();
+break;
+
+case 0: 
+
+break;
+default:
+cout<<"Invalid choice : "<<endl;
+
+
+
+
+
+}
+
+}while(choice!=0);
+
+
+}
+
+
+
 
 int main(){
 
