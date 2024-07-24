4c46d6b2f9964b75a789dfe924c0eaa7463f01f0
haris78866 <arainharis151@gmail.com>
2024-07-24 15:18:54 +0500

    ADD

--- double-circuler-linked-list.cpp
+++ double-circuler-linked-list.cpp
@@ -17,17 +17,101 @@ list();
 ~list();
 void option();
 void search();
-
+void length();
+void add();
+void remove();
+void print();
 };
+
+//constructer 
+list :: list(){
+head=temp=temp1=NULL;
+}
+
+
+
+// destructer 
+list :: ~list(){
+temp=head;
+while(temp->next!=head){
+temp1=temp;
+temp=temp->next;
+delete temp1;
+}
+delete temp;
+head=NULL;
+}
+
+
+void list :: option(){
+int choice;
+do{
+cout<<"Avalible option : "<<endl;
+cout<<"1) add "<<endl;
+cout<<"2) remove "<<endl;
+cout<<"3) print "<<endl;
+cout<<"4) length "<<endl;
+cout<<"5) search "<<endl;
+cout<<"0) Exit ! "<<endl;
+cout<<"Enter your choice : "<<endl;
+cin>>choice;
+
+
+switch(choice){
+
+case 1:
+add(); 
+break;
+
+case 2:
+remove(); 
+break;
+
+case 3:
+print();
+break;
+
+case 4:
+length();
+break;
+
+case 5:
+search();
+break;
+
+case 0:
+break;
+
+default :
+cout<<"Invalid choice :"<<endl;
+     }
+  }while(choice!=0);
+}
+
+
+void list :: print(){
+temp=head;
+if(head==NULL){
+cout<<"No Element to print : "<<endl;
+return;
+}
+
+do{
+cout<<temp->info<<" ";
+temp=temp->next;
+}while(temp!=head);
+cout<<endl;
+}
+
+
+
+void list :: 
 
 
 
 
 
 int main(){
-
-
-
 
 return 0;
 }
