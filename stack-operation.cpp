e0aeb753a456f5c6bd62cd77b54b649f7aa6e435
haris78866 <arainharis151@gmail.com>
2024-07-25 19:08:05 +0500

    ADD

--- stack-operation.cpp
+++ stack-operation.cpp
@@ -162,10 +162,89 @@ node *temp,*top;
 int count;
 int limit;
 public : 
+list();
+~list();
+void option();
+void print();
+void push();
+void pop();
+};
 
 
-
+list :: list(){
+top=temp=NULL;
+count=0;
+limit=10;
 }
+
+
+list :: ~list(){
+temp=top;
+while(top!=NULL){
+temp=temp->next;
+delete temp;
+}
+}
+
+
+
+void list :: option(){
+int choice;
+do{
+cout<<"Avalible option :"<<endl;
+cout<<"1) push "<<endl;
+cout<<"2) pop "<<endl;
+cout<<"3) print "<<endl;
+cout<<"0) EXIT  "<<endl;
+cout<<"Enter  your choice : "<<endl;
+cin>>choice;
+
+
+if(choice==1){
+push();
+}
+else if(choice==2){
+pop();
+}
+else if(choice==3){
+print();
+}
+
+else if(choice==0){
+break;
+}
+
+else{
+cout<<"Invalid Choice : "<<endl;
+    }
+  }while(choice!=0);
+}
+
+
+void list :: push(){
+temp=top;
+
+if(top==NULL){
+top=new node;
+cout<<"Enter the value in the stack  : "<<endl;
+cin>>top->info;
+count++;
+top->next=NULL;
+return;
+}
+
+if(count<=limit){
+temp=new node;
+cout<<"Enter the value in the stack : "<<endl;
+cin>>temp->info;
+temp->next=top;
+top->next=NULL;
+count++;
+top=temp;
+return;
+}
+}
+
 
 int main(){
 
