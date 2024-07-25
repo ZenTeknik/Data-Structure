ff5226222376e8a4cd798380f1c97b87e72a5c18
haris78866 <arainharis151@gmail.com>
2024-07-25 23:21:32 +0500

    ADD

--- queue-data-structure.cpp
+++ queue-data-structure.cpp
@@ -18,6 +18,7 @@ queue();
 void option();
 void nqueue();
 void dqueue();
+void print();
 };
 
 
@@ -42,15 +43,19 @@ do{
 cout<<"Avalible option :"<<endl;
 cout<<"1) insert "<<endl;
 cout<<"2) remove : "<<endl;
+cout<<"3) print : "<<endl;
 cout<<"0) exit"<<endl;
 cout<<"Enter your choice : "<<endl;
 cin>>choice;
 
 if(choice==1){
-
+nqueue();
 }
 else if(choice==2){
-
+dqueue();
+}
+else if(choice==3){
+print();
 }
 else if(choice==0){
 break;
@@ -60,7 +65,7 @@ cout<<"Invalid choice : "<<endl;
 }
 }while(choice!=0);
 }
-}
+
 
 void queue :: nqueue(){
 rear=front;
@@ -73,12 +78,14 @@ front->next=NULL;
 return;
 }
 
+if(count<=limit){
 rear=new node;
 count++;
 cout<<"Enter the first letter of name : "<<endl;
 cin>>rear->info;
 rear->next=NULL;
-rear=front->next;
+rear->next=front;
+}
 }
 
 
@@ -87,14 +94,35 @@ void queue :: dqueue(){
 rear=front;
 
 if(front==NULL){
-
-
+cout<<"No person in the Queue : "<<endl;
+return;
 }
 
+front=front->next;
+delete rear;
+count--;
+return;
+}
+
+
+void queue :: print(){
+rear=front;
+
+if(front==NULL){
+cout<<"No person in the queue : "<<endl;
+return;
+}
+
+while(rear!=NULL){
+cout<<rear->info;
+rear=rear->next;
+}
+cout<<endl;
 }
 
 
 int main(){
-
+queue obj;
+obj.option();
 return 0;
 }
