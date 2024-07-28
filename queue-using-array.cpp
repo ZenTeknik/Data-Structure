228197b2bd28fd74b2b8c2af27f293834ce7d233
haris78866 <arainharis151@gmail.com>
2024-07-28 18:44:33 +0500

    ADD

--- queue-using-array.cpp
+++ queue-using-array.cpp
@@ -1,10 +1,73 @@
 #include<iostream>
 using namespace std;
+int size;
+int front=-1;
+int rear=-1;
+int *arr;
+
+
+void option(){
+int choice; 
+do{
+cout<<"Avalible option : "<<endl;
+cout<<"1) enqueue : "<<endl;
+cout<<"2) dequeue : "<<endl;
+cout<<"3) peek : "<<endl;
+cout<<"4) print : "<<endl;
+cout<<"0) exit : "<<endl;
+cout<<"Enter your choice :"<<endl;
+cin>>choice;
+if(choice==1){
+
+}
+else if(choice==2){
+
+}
+else if(choice==3){
+
+}
+else if(choice==4){
+
+}
+else if(choice==0){
+break;
+}
+else {
+cout<<"Invalid choice : "<<endl;
+}
+
+}while(choice!=0);
+}
+
+void enqueu(int x){
+if(rear==size-1){
+cout<<"Queue is Full : "<<endl;
+return;
+}
+else if(front==-1 && rear==-1){
+front=rear=0;
+arr[rear]=x;
+return;
+}
+else if()
+
+
+
+
+
+}
 
 
 
 int main(){
 
 
+cout<<"Enter the size of an array  : "<<endl;
+cin>>size;
+
+arr=new int[size];
+
+option();
+
 return 0;
-}
+}nt size
