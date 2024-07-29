f99910bf71dc195b499bb61627654964e2dbf32d
haris78866 <arainharis151@gmail.com>
2024-07-29 22:47:30 +0500

    ADD

--- binary-tree.cpp
+++ binary-tree.cpp
@@ -11,13 +11,27 @@ node(int val ){
 info=val;
 left=NULL;
 right=NULL;
+
+~node{
+delete left;
+delete right;
+}
+
 };
 
 
 
 int main(){
 
+struct node *root=  new node(1);
+root->left=  new node(2);
+root->right=  new node(3);
 
+root->left->left =new node(4);
+root->left->right=NULL;
+root->right->left=new node(5);
+root->right->right=new node(6);
 
+delete root;
 return 0;
 }
