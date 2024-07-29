86b6e33c36c7ebfcb2c5cc66d49fa1cb38166b18
haris78866 <arainharis151@gmail.com>
2024-07-29 23:40:33 +0500

    ADD

--- binary-tree.cpp
+++ binary-tree.cpp
@@ -68,3 +68,77 @@ return 0;
 
 
 */
+
+
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+int   info;
+node *left;
+node *right;
+node(int info){
+this->info=info;
+left=NULL;
+right=NULL;
+}
+
+~node(){
+if(left){
+delete left;
+left=NULL;
+}
+if(right){
+delete right;
+right=NULL;
+}
+}
+};
+
+
+
+void preorder(node * root){
+if(root==NULL){
+cout<<"-1 ";
+}
+else{
+cout<<root->info<<" ";
+preorder(root->left);
+preorder(root->right);
+}
+}
+
+void postorder(node *root){
+if(root==NULL){
+cout<<" -1 ";
+}
+else {
+postorder(root->left);
+postorder(root->right);
+cout<<root->info<<" "; 
+}
+}
+
+int main(){
+
+ node *root=new node(1);
+root->left=new node(2);
+root->left->left=new node(3);
+root->right=new node(4);
+root->right->left=new node(5);
+root->right->right=new node(6);
+
+
+// for printing the preorder of the tree like fron top to down 
+preorder(root);
+cout<<endl;
+// for printing the postorder of the tree like from down to top
+postorder(root);
+
+
+
+delete root;
+
+return 0;
+}
