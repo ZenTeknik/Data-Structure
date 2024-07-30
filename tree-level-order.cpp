36f0ba7d355fe5bc99d12a860bed6efb6d4b400d
haris78866 <arainharis151@gmail.com>
2024-07-30 23:32:31 +0500

    ADD

--- tree-level-order.cpp
+++ tree-level-order.cpp
@@ -150,11 +150,12 @@ int main(){
 node *root=new node(1);
 root->left=new node(2);
 root->right=new node(3);
-root->left->right=new node(4);
-root->left->left=new node(5);
+
 root->right->left=new node(6);
 root->right->right=new node(7);
 
+root->left->right=new node(4);
+root->left->left=new node(5);
 
 levelorder(root);
 cout<<endl;
