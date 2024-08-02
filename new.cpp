727518ae1de2cd21b59669376b1d79fcf26dada0
haris78866 <arainharis151@gmail.com>
2024-08-02 22:05:44 +0500

    ADD

--- new.cpp
+++ new.cpp
@@ -37,11 +37,73 @@ tree :: ~tree(){
 deletion(root);
 }
 
+// fcuntion for the destructer 
 void tree :: deletion(node *root){
-
-
+if(root!=NULL){
+deletion(root->left);
+deletion(root->right);
+delete root;
 }
 
+
+void tree :: option(){
+int choice,key;
+do{
+cout<<"Avalible option : "<<endl;
+cout<<"1) Insert "<<endl;
+cout<<"2) Preorder "<<endl;
+cout<<"3) Postorder "<<endl;
+cout<<"4) Inorder "<<endl;
+cout<<"5) Levelorder "<<endl;
+cout<<"0) Exit "<<endl;
+cout<<"Enter your choice : "<<endl;
+cin>>choice;
+
+if(choice==1){
+insert(root);
+cout<<endl;
+}
+else if(choice==2){
+preorder(root);
+cout<<endl;
+}
+else if(choice==3){
+postorder(root);
+cout<<endl;
+}
+else if(choice==4){
+inorder(root);
+cout<<endl;
+}
+else if(choice==5){
+levelorder(root);
+cout<<endl;
+}
+else if(choice==0){
+break;
+}
+else{
+cout<<"Invalid choice : "<<endl;
+}
+}while(choice!=0);
+}
+
+
+
+void insert(node *&root,int key){
+if(root==NULL){
+root=new node(key);
+return;
+}
+
+if(key==root->info){
+cout<<"Key is already in Tree : "<<endl;
+return;
+}
+
+if(key<)
+
+}
 
 int main(){
 
