7f13b1aa53a0f0195b2a3f1998dc2487a950e1a0
haris78866 <arainharis151@gmail.com>
2024-08-02 21:48:52 +0500

    ADD

---
+++ new.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+
+struct node{
+int info;
+node *right,*left;
+
+node(int val) : val(info), left(NULL),right(NULL){};
+};
+
+
+
+class tree{ 
+private: 
+node *root;
+public:
+tree();
+~tree();
+void deletion(node *root);
+void insert(node *&root,int key );
+void 
+
+};
+
+int main(){
+
+
+
+return 0;
+}
+
