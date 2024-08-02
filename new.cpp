0d9b9b7c791c166e69e63355f771212263934524
haris78866 <arainharis151@gmail.com>
2024-08-02 20:35:56 +0500

    ADD

--- new.cpp
+++ new.cpp
@@ -5,6 +5,8 @@ using namespace std;
 struct node{
 int info;
 node *left,right;
+
+node(int val) : info(val) , left(NULL), right(NULL){};
 };
 
 class tree{
@@ -20,6 +22,65 @@ void insert(key);
 void preorder(node *root);
 };
 
+
+
+tree :: tree(){
+root=NULL;
+}
+
+tree :: ~tree(){
+deletetion(root);
+}
+
+void tree :: deletetion(node *root){
+if(root!=NULL){
+deletetion(root->left);
+deletetion(root->right);
+delete root;
+}
+}
+
+
+void tree :: option(){
+int choice;
+int key;
+do{
+cout<<"Avalible option : "<<endl;
+cout<<"1) Insert  "<<endl;
+cout<<"2) Preorder  "<<endl;
+cout<<"0) Exit  "<<endl;
+cout<<"Enter your choice : "<<endl;
+cin>>choice;
+
+if(choice==1){
+cout<<"Enter the key u have to insert : "<<endl;
+cin>>key;
+insert(key);
+}
+
+else if(choice==2){
+preorder(root);
+}
+
+else if(choice==0){
+cout<<"Exiting the programm : "<<endl;
+break;
+}
+
+else{
+cout<<"Invalid choice : "<<endl;
+}
+
+}while(choice!=0);
+}
+
+
+
+void preorder(node *root){
+
+
+
+}
 
 
 
