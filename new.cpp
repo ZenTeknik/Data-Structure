812e16842b9c9648e21d8a25507cc380c6636e91
haris78866 <arainharis151@gmail.com>
2024-08-02 13:07:34 +0500

    ADD

---
+++ new.cpp
@@ -0,0 +1,29 @@
+#include<iosrteam>
+#include<queue>
+using namespace std;
+
+struct node{
+int info;
+node *left,right;
+};
+
+class tree{
+private:
+node *root;
+public:
+
+tree();
+~tree();
+void deleteion();
+void option();
+
+
+
+};
+
+int main(){
+
+
+
+return 0;
+}
