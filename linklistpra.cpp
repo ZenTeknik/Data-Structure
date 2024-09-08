e15b455ea4bcde34422d2362ce8cca73173d2a12
haris78866 <arainharis151@gmail.com>
2024-09-08 19:34:01 +0500

    ADD

---
+++ linklistpra.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+int info;
+node *next;
+};
+
+
+
+class list{
+private:
+node *head,*temp*temp1;
+public:
+
+list();
+~list();
+void option();
+void search();
+void add();
+void remove();
+void length();
+void 
+
+
+};
+
+
+
+
+int main(){
+
+
+
+
+return 0;
+}
