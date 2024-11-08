74e35d7d14e108af7b09aabfc35ebb58cf55c7e0
Haris-Shahzad <144903716+ZenTeknik@users.noreply.github.com>
2024-11-08 06:10:27 +0500

    Add files via upload

--- josephprob.cpp
+++ josephprob.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 
+
 struct node{
-int  info;
+int info;
 node *next;
 };
 
-class list {
+
+class list{
 private:
 node *head,*temp,*temp1;
 int listsize=0;
@@ -17,35 +19,35 @@ void option();
 void add();
 void remove();
 void print();
+void search();
 };
 
 
+
 list :: list(){
-head=temp=temp1=NULL;
+head=temp=temp1;
 }
 
 
 list :: ~list(){
 temp=head;
-do{
+while(temp->next!=head){
 temp1=temp;
 temp=temp->next;
 delete temp1;
-}while(temp!=head);
 }
-
-
-
+}
 
 
 void list :: option(){
 int choice;
 do{
-cout<<"Avalible option : "<<endl;
-cout<<"1) add    :  "<<endl;
-cout<<"2) remove :  "<<endl;
-cout<<"3) print  : "<<endl;
-cout<<"0) EXIT   : "<<endl;
+cout<<"Avalible Choices : "<<endl;
+cout<<"1)  Add : "<<endl;
+cout<<"2)  Remove : "<<endl;
+cout<<"3)  Print  : "<<endl;
+cout<<"4)  Search : "<<endl;
+cout<<"0)  Exit : "<<endl;
 cout<<"Enter your choice : "<<endl;
 cin>>choice;
 
@@ -58,25 +60,32 @@ remove();
 else if(choice==3){
 print();
 }
+
+else if(choice==4){
+search();
+}
+
 else if(choice==0){
 break;
 }
+
 else{
-cout<<"Invalid Option : "<<endl;
+cout<<"Invalid Choice : "<<endl;
 }
 }while(choice!=0);
 }
 
 
 
-// function for add 
+
 void list :: add(){
 temp=head;
 
-if(head==NULL){
-head=new node;
+while(head==NULL){
+head =new node;
 cout<<"Enter the info : "<<endl;
 cin>>head->info;
+
 head->next=head;
 listsize++;
 return;
@@ -89,88 +98,83 @@ temp=temp->next;
 temp1=new node;
 cout<<"Enter the info : "<<endl;
 cin>>temp1->info;
-
 temp1->next=head;
 temp->next=temp1;
 listsize++;
 }
 
 
-
-
-
-
-
-
-
-
-// function for removing the  node
 void list :: remove(){
-node *current=head;
-node *prev=NULL;
-
+temp=head;
 if(head==NULL){
 cout<<"Empty list : "<<endl;
 return;
 }
 
-int k;
-cout<<"Enter the k : "<<endl;
-cin>>k;
+int k,count=1;
+cout<<"Enter the K value (Joseph problem : )"<<endl;
+cin>>key;
 
-while(listsize>1){
-      
-      for(int i=1;i<k;i++){
-      prev=current;
-      current=current->next;
+
+
+while(temp->next!=head){
+    if(count==k){
+  temp->next=temp1;
+  temp->next->next=temp->next;
+  delete temp1;
+  listsize--;
+  return;
+}
+temp=temp->next;
+count++;
 }
 
-cout<<"Removing nodes : "<<current->info<<endl;
-
-if(current==head){
-head=head->next;
-}
-
-prev->next=current->next;
-delete current;
-current=prev->next;
-listsize--;
-}
-cout<<"Last node : "<<head->info<<endl;
-}
 
 
 
 void list :: print(){
-
+temp=head;
 
 if(head==NULL){
 cout<<"Empty list : "<<endl;
 return;
 }
 
-temp=head;
-do{
-   cout<<temp->info<<" ";
-   temp=temp->next;
-   }while(temp!=head);
-   cout<<endl;
+while(temp->next!=head){
+cout<<temp->info<<" ";
+temp=temp->next;
+}
+cout<<endl;
 }
 
 
+void list :: search(){
+temp=head;
+int key;
+if(head==NULL){
+cout<<"Empty list : "<<endl;
+return;
+}
 
+cout<<"Enter the key to search : "<<endl;
+cin>>key;
 
-
-
-
+while(temp->next!=head){
+     if(temp->info==key){
+cout<<"Key exist : "<<endl;
+}
+temp=temp->next;
+}
+cout<<"Key not exist : "<<endl;
+}
 
 
 
 
 
 int main(){
-list obj;
-obj.option();
 
 
+
+return 0;
 }
