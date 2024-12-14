8d297daea0c7d58c9c3c0759e3640e46e79c7ac5
Haris-Shahzad <144903716+ZenTeknik@users.noreply.github.com>
2024-12-14 22:49:06 +0500

    Add files via upload

---
+++ threaded-binary-tree.cpp
@@ -0,0 +1,5 @@
+   // if the left & right pointer  of any node is NULL
+   //     in single threaded binary tree is the right node is null  then it store the address of the inorder successor
+   //
+   //     In double threaded  binary tree   if the left pointer is NULL then its store the  address of the  inorder pred successor
+   //
